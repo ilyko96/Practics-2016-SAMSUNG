445d2d353fb31ae093e5d0c07f349a17187a1a95
ilyko96 <vfpkjd@gmail.com>
2016-07-13 12:10:08 +0300

    Worked with menu panel

--- projectPillSched/js/main.js
+++ projectPillSched/js/main.js
@@ -1,21 +1,5 @@
-window.onload = function() {
-//    // TODO:: Do your initialization job
-//
-//    // add eventListener for tizenhwkey
-//    document.addEventListener('tizenhwkey', function(e) {
-//        if (e.keyName === "back") {
-//            try {
-//                tizen.application.getCurrentApplication().exit();
-//            } catch (ignore) {}
-//        }
-//    });
-//
-//    // Sample code
-//    var mainPage = document.querySelector('#main');
-//
-//    mainPage.addEventListener("click", function() {
-//        var contentText = document.querySelector('#content-text');
-//
-//        contentText.innerHTML = (contentText.innerHTML === "Basic") ? "Tizen" : "Basic";
-//    });
-};
+$(document).ready(function(){
+	$('#btn-page-month').click(function() {
+		$.mobile.changePage('#page-month');
+	});
+});
